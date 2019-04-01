c6453c4ccbfeab952230a8741c7558a10dd58c78
Ryan Jeff Brual <lrjbrual@live.com>
2019-04-01 18:46:45 +0200

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'event/index'
+  get 'event/show'
+  get 'event/new'
+  get 'event/create'
+  get 'event/destroy'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
